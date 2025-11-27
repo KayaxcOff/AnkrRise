50efa8c9155ee5dc5575d3916caa49ddb3480e2f
Muhammet Kaya <muham123cak@gmail.com>
2025-11-27 15:44:55 +0300

    change: trying write characters

--- MindEngine2D/Loop/Character/champion/champion.hpp
+++ MindEngine2D/Loop/Character/champion/champion.hpp
@@ -4,8 +4,9 @@
 
 class Characters : public Champion {
 public:
-	Characters : public Champion ();
-	~Characters : public Champion ();
+	Characters(const std::string& _name, int _hp, int _maxHp, int _attack, std::string& _file, b2Body* _body) 
+		: Champion(_name, _hp, _maxHp, _attack, _file, _body) {}
+	~Characters() {}
 
 private:
 
